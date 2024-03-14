39eb654d9b02423c1f5fac7c1c1a11495ea0d671
tuxilio <tuxilio@mailbox.org>
2024-03-14 16:03:18 +0100

    Start empty to dont spend 5 MB to commit history

--- README.md
+++ README.md
@@ -1,3 +1,107 @@
-# forgejo-stats
+# [Forgejo / Gitea Stats Visualization](https://codeberg.org/Tuxilio/forgejo-stats/)
 
-Statistic images for your profile, with stats from private repos too
+<!--
+https://github.community/t/support-theme-context-for-images-in-light-vs-dark-mode/147981/84
+-->
+<a href="https://codeberg.org/Tuxilio/forgejo-stats">
+<img src="generated/overview.svg#gh-dark-mode-only"/>
+<img src="generated/languages.svg#gh-dark-mode-only"/>
+<br>
+<img src="generated/overview.svg#gh-light-mode-only"/>
+<img src="generated/languages.svg#gh-light-mode-only"/>
+</a>
+
+Generate visualizations of Forgejo / Gitea user and repository statistics with Forgejo / Gitea Actions. Visualizations can include data for private repositories.
+
+You can choose between like and dark mode.
+
+## Background
+
+When someone views a profile, it is often because they are curious about a user's open source projects and contributions. Unfortunately, that user's stars, forks, and pinned repositories do not necessarily reflect the contributions they make.
+
+This project aims to collect a variety of profile and repository statistics using the Forgejo / Gitea API. It then generates images that can be displayed in repository READMEs, or in a user's [Profile README](https://forgejo.org/docs/latest/user/profile/).
+
+There are many GitHub statistics visualizations for GitHub. But also at Forgejo or Gitea some are interested in the number of stars of a user, the most used programming languages or the changed lines.
+
+This project is still under development, but it's a good start. It is already possible to create simple visualizations.
+
+This project uses [GitHub stats](https://github.com/jstrieb/github-stats) by [Jacob Strieb](https://github.com/jstrieb) (licensed under GPL 3.0 or later) as base and uses the [Language Colors](https://github.com/ozh/github-colors) by [OZH](https://github.com/ozh) (licensed under WTFPL) for the language color visualizations.
+
+Since the project runs on Forgejo / Gitea Actions, no server is required to regularly regenerate the images with updated statistics. Likewise, since the user runs the analysis code themselves via Forgejo / Gitea Actions, they can use their access token to collect statistics on private repositories that an external service would be unable to access.
+
+## Installation
+
+<!-- TODO: Add details and screenshots -->
+
+1. Create a personal access token using the instructions [here](https://docs.codeberg.org/advanced/access-token/). Personal access token must have permissions: `Read - repository` and `Read - user`. Copy the access token when it is generated – if you lose it, you will have to
+   regenerate the token.
+<!-- TODO: Add explaination -->
+   <!--
+2. Create a copy of this repository by clicking
+   [here](https://github.com/jstrieb/github-stats/generate). Note: this is
+   **not** the same as forking a copy because it copies everything fresh,
+   without the huge commit history. 
+3. Go to the "Secrets" page of your copy of the repository. If this is the
+   README of your copy, click [this link](../../settings/secrets/actions) to go
+   to the "Secrets" page. Otherwise, go to the "Settings" tab of the
+   newly-created repository and go to the "Secrets" page (bottom left).
+4. Create a new secret with the name `ACCESS_TOKEN` and paste the copied
+   personal access token as the value.
+5. It is possible to change the type of statistics reported by adding other
+   repository secrets. 
+   - To ignore certain repos, add them (in owner/name format e.g.,
+     `jstrieb/github-stats`) separated by commas to a new secret—created as
+     before—called `EXCLUDED`.
+   - To ignore certain languages, add them (separated by commas) to a new
+     secret called `EXCLUDED_LANGS`. For example, to exclude HTML and TeX you
+     could set the value to `html,tex`.
+   - To show statistics only for "owned" repositories and not forks with
+     contributions, add an environment variable (under the `env` header in the
+     [main
+     workflow](https://github.com/jstrieb/github-stats/blob/master/.github/workflows/main.yml))
+     called `EXCLUDE_FORKED_REPOS` with a value of `true`.
+   - These other values are added as secrets by default to prevent leaking
+     information about private repositories. If you're not worried about that,
+     you can change the values directly [in the Actions workflow
+     itself](https://github.com/jstrieb/github-stats/blob/05de1314b870febd44d19ad2f55d5e59d83f5857/.github/workflows/main.yml#L48-L53).
+6. Go to the [Actions
+   Page](../../actions?query=workflow%3A"Generate+Stats+Images") and press "Run
+   Workflow" on the right side of the screen to generate images for the first
+   time. 
+   - The images will be automatically regenerated every 24 hours, but they can
+     be regenerated manually by running the workflow this way.
+7. Take a look at the images that have been created in the
+   [`generated`](generated) folder.
+8. To add your statistics to your GitHub Profile README, copy and paste the
+   following lines of code into your markdown content. Change the `username`
+   value to your GitHub username.
+   ```md
+   ![](https://raw.githubusercontent.com/username/github-stats/master/generated/overview.svg#gh-dark-mode-only)
+   ![](https://raw.githubusercontent.com/username/github-stats/master/generated/overview.svg#gh-light-mode-only)
+   ```
+   ```md
+   ![](https://raw.githubusercontent.com/username/github-stats/master/generated/languages.svg#gh-dark-mode-only)
+   ![](https://raw.githubusercontent.com/username/github-stats/master/generated/languages.svg#gh-light-mode-only)
+   ```-->
+9. Link back to this repository so that others can generate their own
+   statistics images.
+10. Star this repo if you like it!
+
+
+# Support the Project
+
+There are a few things you can do to support the project:
+
+- Star the repository (and follow me on Codeberg for more)
+- Report any bugs, glitches, or errors that you find
+
+These things motivate me to to keep sharing what I build, and they provide validation that my work is appreciated! They also help me improve the project. Thanks in advance!
+
+# Related Projects
+
+- Inspired by [GitHub stats](https://github.com/jstrieb/github-stats) by [Jacob Strieb](https://github.com/jstrieb)
+- Inspired by a desire to improve upon [anuraghazra/github-readme-stats](https://github.com/anuraghazra/github-readme-stats)
+- Makes use of [GitHub Octicons](https://primer.style/octicons/)
+
+> **Info** Wondering why there are so few commits?<br>
+I first migrated Jacob Strie's project from GitHub to fork it and have it as a base but then the commit history was such large - so I deleted the repo and uploaded just the files to have an empty commit history.
---
+++ main.py
@@ -0,0 +1,360 @@
+#!/usr/bin/python3
+
+"""
+    Forgejo / Gitea Stats Generator
+    Copyright (C) 2022 Jacob Strieb
+    Copyright (C) 2024 Tuxilio
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+import re
+from typing import Dict, List, Optional, Set, Tuple, Any, cast
+import json
+import sys
+
+import requests
+
+
+###############################################################################
+# Main Classes
+###############################################################################
+
+
+class Queries(object):
+    """
+    Class with functions to query the Forgejo / Gitea API.
+    """
+
+    def __init__(
+        self,
+        username: str,
+        access_token: str,
+        git_url: str,
+        max_connections: int = 10,
+    ):
+        self.username = username
+        self.access_token = access_token
+        self.git_url = git_url
+
+    def query(self, query: str) -> Dict:
+        """
+        Make a request to the Forgejo / Gitea API using the authentication token from
+        the environment
+        :param generated_query: string query to be sent to the API
+        :return: JSON output
+        """
+        headers = {
+            "accept": "application/json"
+        }
+
+        generated_query = self.git_url + "/api/v1" + query + "?token=" + self.access_token
+
+        response = requests.get(generated_query, headers=headers)
+
+        if response.status_code != 200:
+            print("Error while making the request. Status code:", response.status_code)
+            sys.exit(1)
+
+        return response.json()
+
+class Stats(object):
+    """
+    Retrieve and store statistics about Forgejo / Gitea usage.
+    """
+
+    def __init__(
+        self,
+        username: str,
+        access_token: str,
+        git_url: str,
+        exclude_repos: Optional[Set] = None,
+        exclude_langs: Optional[Set] = None,
+        ignore_forked_repos: bool = False,
+    ):
+        self.username = username
+        self.git_url = git_url
+        self._ignore_forked_repos = ignore_forked_repos
+        self._exclude_repos = set() if exclude_repos is None else exclude_repos
+        self._exclude_langs = set() if exclude_langs is None else exclude_langs
+        self.queries = Queries(username, access_token, git_url)
+
+        self._name: Optional[str] = None
+        self._stargazers: Optional[int] = None
+        self._forks: Optional[int] = None
+        self._total_contributions: Optional[int] = None
+        self._languages: Optional[Dict[str, Any]] = None
+        self._repos: Optional[Set[str]] = None
+        self._repo_list: Optional[Set[str]] = None
+        self._lines_changed: Optional[Tuple[int, int]] = None
+        self._views: Optional[int] = None
+
+    def get_stats(self) -> None:
+        """
+        Get lots of summary statistics
+        """
+
+        self.name()
+        self.repo_list()
+        self.stargazers()
+        self.forks()
+        self.contributions()
+        self.languages()
+
+        print_data = [
+            ["User:", self._name],
+            ["Repos:", self._repo_list],
+            ["Stars:", self._stargazers],
+            ["Forks:", self._forks],
+            ["Lines changed:", self._lines_changed],
+            ["Contributions:", self._total_contributions],
+            ["Languages:", self._languages]
+        ]
+
+        max_len = max(len(row[0]) for row in print_data)
+
+        for row in print_data:
+            print(row[0].ljust(max_len) + "     " + str(row[1]))
+
+    def name(self) -> str:
+        """
+        :return: Forgejo / Gitea user's name (e.g., Tuxilio)
+        """
+        if self._name is not None:
+            return self._name
+
+        response =  self.queries.query("/users/" + self.username)
+
+        self._name = response['full_name']
+
+        if self._name is None or self._name == "":
+            self._name = self._name = response['username']
+
+        return self._name
+
+    def repo_list(self) -> str:
+        """
+        :return: Forgejo / Gitea repo list (e.g., [repo1, repo2])
+        """
+        if self._repo_list is not None:
+            return self._repo_list
+
+        self._repo_list = []
+
+        response =  self.queries.query("/users/" + self.username + "/repos")
+
+        for repo in response:
+            self._repo_list.append(repo['name'])
+
+        return self.repo_list
+
+    def stargazers(self) -> str:
+        """
+        :return: Forgejo / Gitea total stargazers (e.g., 24)
+        """
+        if self._stargazers is not None:
+            return self._stargazers
+
+        self._stargazers = 0
+
+        for i in self._repo_list:
+            response =  self.queries.query("/repos/" + self.username + "/" + i + "/stargazers")
+
+            for a in response:
+                self._stargazers += 1
+
+        return self._stargazers
+
+    def forks(self) -> str:
+        """
+        :return: Forgejo / Gitea total forks (e.g., 12)
+        """
+        if self._forks is not None:
+            return self._forks
+
+        self._forks = 0
+
+        for i in self._repo_list:
+            response =  self.queries.query("/repos/" + self.username + "/" + i + "/forks")
+
+            for a in response:
+                self._forks += 1
+
+        return self._forks
+
+    def contributions(self) -> str:
+        """
+        :return: Forgejo / Gitea total lines changed (e.g., 35011)
+        :return: Forgejo / Gitea total contributions (e.g., 514)
+        """
+        if self._lines_changed is not None and self._total_contributions is not None:
+            return self._lines_changed, self._total_contributions
+
+        self._lines_changed = 0
+        self._total_contributions = 0
+
+        for i in self._repo_list:
+            response =  self.queries.query("/repos/" + self.username + "/" + i + "/commits")
+
+            for a in response:
+                self._total_contributions += 1
+                self._lines_changed += a['stats']['total']
+
+        return self._lines_changed, self._total_contributions
+
+    def languages(self) -> str:
+        """
+        :return: Most used languages (e.g., 35011)
+        """
+        if self._languages is not None:
+            return self._languages
+
+        language_data = {}
+
+        for i in self._repo_list:
+            response =  self.queries.query("/repos/" + self.username + "/" + i + "/languages")
+
+            for language, bytes in response.items():
+                if language in language_data:
+                    language_data[language] += bytes
+                else:
+                    language_data[language] = bytes
+
+        total_bytes = sum(language_data.values())
+
+        self._languages = {language: round(bytes / total_bytes * 100, 2) for language, bytes in language_data.items()}
+
+        return self._languages
+
+class Generate(object):
+    def __init__(
+        self,
+        stats,
+        exclude_repos: Optional[Set] = None,
+        exclude_langs: Optional[Set] = None,
+        ignore_forked_repos: bool = False,
+    ):
+        self.stats = stats
+
+    def generate_output_folder(self) -> None:
+        """
+        Create the output folder if it does not already exist
+        """
+        if not os.path.isdir("generated"):
+            os.mkdir("generated")
+
+    def generate_overview(self) -> None:
+        """
+        Generate an SVG badge with summary statistics
+        :param s: Represents user's GitHub statistics
+        """
+        with open("templates/overview.svg", "r") as f:
+            output = f.read()
+
+        output = re.sub("{{ name }}", self.stats._name, output)
+        output = re.sub("{{ stars }}", f"{self.stats._stargazers:,}", output)
+        output = re.sub("{{ forks }}", f"{self.stats._forks:,}", output)
+        output = re.sub("{{ contributions }}", f"{self.stats._total_contributions:,}", output)
+        changed = sum(self.stats._lines_changed) if isinstance(self.stats._lines_changed, tuple) else self.stats._lines_changed
+        output = re.sub("{{ lines_changed }}", f"{changed:,}", output)
+
+        """views_str = f"{self.stats._views:,}" if self.stats._views is not None else "N/A"
+        output = re.sub("{{ views }}", views_str, output)
+        repos_len = len(self.stats._repos) if self.stats._repos is not None else 0
+        output = re.sub("{{ repos }}", f"{repos_len:,}", output)"""
+
+        self.generate_output_folder()
+        with open("generated/overview.svg", "w") as f:
+            f.write(output)
+
+
+    def generate_languages(self) -> None:
+        """
+        Generate an SVG badge with summary languages used
+        :param s: Represents user's GitHub statistics
+        """
+        with open("templates/languages.svg", "r") as f:
+            output = f.read()
+
+        with open('colors.json') as d:
+            color_data = json.load(d)
+
+
+        progress = ""
+        lang_list = ""
+
+        # Sort languages based on their percentage
+        sorted_languages = sorted(
+            self.stats._languages.items(),
+            reverse=True,
+            key=lambda t: t[1]  # Sort based on the language percentage directly
+        )
+
+        delay_between = 150
+        for i, (lang, data) in enumerate(sorted_languages):
+            if lang in color_data:
+                color = color_data[lang]['color']
+            else:
+                color = "#000000"
+
+            progress += (
+                f'<span style="background-color: {color};'
+                f'width: {data:0.3f}%;" '
+                f'class="progress-item"></span>'
+            )
+            lang_list += f"""
+<li style="animation-delay: {i * delay_between}ms;">
+<svg xmlns="http://www.w3.org/2000/svg" class="octicon" style="fill:{color};"
+viewBox="0 0 16 16" version="1.1" width="16" height="16"><path
+fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
+<span class="lang">{lang}</span>
+<span class="percent">{data:0.2f}%</span>
+</li>
+"""
+
+        output = re.sub(r"{{ progress }}", progress, output)
+        output = re.sub(r"{{ lang_list }}", lang_list, output)
+
+        self.generate_output_folder()
+        with open("generated/languages.svg", "w") as f:
+            f.write(output)
+
+###############################################################################
+# Main Function
+###############################################################################
+
+
+def main() -> None:
+    """
+    Used mostly for testing; this module is not usually run standalone
+    """
+    access_token = "TOKEN"
+    user = "tuxilio"
+    git_url = "https://codeberg.org"
+
+    if access_token is None or user is None:
+        raise RuntimeError(
+            "ACCESS_TOKEN and GITHUB_ACTOR environment variables cannot be None!"
+        )
+
+    stats = Stats(user, access_token, git_url)
+    stats.get_stats()
+
+    generate = Generate(stats)
+    generate.generate_overview()
+    generate.generate_languages()
+
+if __name__ == "__main__":
+    main()
